3909caf63acf34f7eb784c2a59734a749a5d686c
Ty Saltzgiver <118573450+Saltzmaster@users.noreply.github.com>
2024-09-27 10:49:22 -0600

    fixed dark mode img

--- mission/mission.js
+++ mission/mission.js
@@ -1,6 +1,14 @@
 const darkMode = document.getElementById('darkMode');
 const body = document.body;
+const logo = document.getElementById('logo');
 
 darkMode.addEventListener('click', function() {
     body.classList.toggle('dark')
+    
+    if (body.classList.contains('dark')) {
+        logo.src = 'byui-logo_dark.png';
+    }
+    else {
+        logo.src = 'byui-logo.webp';
+    }
 });
